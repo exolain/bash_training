6766afd454d88fd048becf5996310dbb8bdd738b
exolain <nmarinpe@gmail.com>
2016-11-10 08:14:10 -0600

    organizing instructions

--- bash_review.sh
+++ bash_review.sh
@@ -10,13 +10,21 @@ This script will execute a lot of random stuff.
 
 EOF
 }
-# 0. Function name: logging. Create a logging function that takes a positional parameter as the message to be appended to the script.log file. The message should have the format: date(year-month-day) - $MESSAGE - running as the "user running the script"
+# 0. Function name: logging. 
+#   Create a logging function that takes a positional parameter as the message to be appended to the script.log file.
+#   The message should have the format: date(year-month-day) - $MESSAGE - running as the "user running the script"
 
-# 1. Function name: reverse_list. Create a function that takes the /etc/passwd file users and display it in reverse order, using arrays and loop. 
+# 1. Function name: reverse_list. 
+#    Create a function that takes the /etc/passwd file users and display it in reverse order, using arrays and loop. 
 
-# 2. Function name: harden_ssh. Copy the /etc/ssh/sshd_config file into the current user home directory. Replace the string ServerKeyBits 1024 to ServerKeyBits 4096. After replacing it, log the message using the logging function and also concatenate the exit code
+# 2. Function name: harden_ssh. 
+#    Copy the /etc/ssh/sshd_config file into the current user home directory. 
+#    Replace the string ServerKeyBits 1024 to ServerKeyBits 4096. 
+#    After replacing it, log the message using the logging function and also concatenate the exit code
 
-#3 Function name: remove_sensitive_files. Create a file called "sensitive_file" and then request confirmation from the user (using the read function), if the user inputs 'y' then remove the file. if not then display the message "No action taken"
+#3 Function name: remove_sensitive_files. 
+#  Create a file called "sensitive_file" and then request confirmation from the user (using the read function), 
+# if the user inputs 'y' then remove the file. if not then display the message "No action taken"
 
 #4 Function name: Factorial. Create a function to calculate factorial. It will receive the number as a positional parameter.
 
